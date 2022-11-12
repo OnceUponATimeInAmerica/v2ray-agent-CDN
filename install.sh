4312e2398cac729525d4c49a64737e4eba934806
OnceUponATimeInAmerica <114700833+OnceUponATimeInAmerica@users.noreply.github.com>
2022-11-12 09:19:03 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3215,9 +3215,17 @@ EOF
 		echoContent yellow " ---> 格式化明文(VLESS+WS+TLS)"
 		echoContent green "    协议类型:VLESS，地址:${currentAdd}，伪装域名/SNI:${currentHost}，端口:${currentDefaultPort}，用户ID:${id}，安全:tls，传输方式:ws，路径:/${currentPath}ws，账户名:${email}\n"
 
+		# OnceUponATimeInAmerica_____ Handle Non-CDN (direct)
 		cat <<EOF >>"/etc/v2ray-agent/subscribe_tmp/${subAccount}"
-vless://${id}@${currentAdd}:${currentDefaultPort}?encryption=none&security=tls&type=ws&host=${currentHost}&sni=${currentHost}&path=/${currentPath}ws#${email}
+vless://${id}@${currentHost}:${currentDefaultPort}?encryption=none&security=tls&type=ws&host=${currentHost}&sni=${currentHost}&path=/${currentPath}ws#${email}
 EOF
+
+		# OnceUponATimeInAmerica_____ Handle CDN
+				cat <<EOF >>"/etc/v2ray-agent/subscribe_tmp/${subAccount}"
+vless://${id}@${currentAdd}:${currentDefaultPort}?encryption=none&security=tls&type=ws&sni=${currentAdd}&path=/${currentPath}ws#CDN_${email}
+EOF
+
+
 
 		echoContent yellow " ---> 二维码 VLESS(VLESS+WS+TLS)"
 		echoContent green "    https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless%3A%2F%2F${id}%40${currentAdd}%3A${currentDefaultPort}%3Fencryption%3Dnone%26security%3Dtls%26type%3Dws%26host%3D${currentHost}%26sni%3D${currentHost}%26path%3D%252f${currentPath}ws%23${email}"
